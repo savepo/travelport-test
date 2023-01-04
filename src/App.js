7d45255053d4815186eef48dc401dfde3d07f845
savepo <savepo2021@bemen3.cat>
2023-01-04 11:56:40 +0100

    Part 3 - Done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import './App.css';
 import PersonCard from './components/PersonCard/index'
 import Button from './components/Button';
-import { data } from './AppMockItems/peopleInformation'
-import { useState } from 'react';
+// import { data } from './AppMockItems/peopleInformation'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 
 let index = 0
+const baseURL = "https://randomuser.me/api/?results=5"
 
 function App() {
   const [ComponentsList, setNewComponent] = useState([])
+  const [post, setPost] = useState(null)
+
+  useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data.results);
+    });
+  }, []);
+
+  console.log(post)
 
   const createNewComponent = () => {
-    if (index === data.length) {
+    if (index === post.length) {
       setNewComponent([])
       index = 0
     } else {
-      setNewComponent([...ComponentsList, data[index]])
+      setNewComponent([...ComponentsList, post[index]])
       index++
     }
   }
